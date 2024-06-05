f7556fa985361c099a2a1547e59cf1c19e5ab352
MarcyBigrock <m.michelotti@hotmail.it>
2024-06-05 15:40:21 +0200

    UP - Cell prefab and grid amanger implementation

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -4,30 +4,41 @@ using UnityEngine;
 public class Cell : MonoBehaviour
 {
     [SerializeField] private List<TileObject> allTiles = new();
-    public List<TileObject> Entropy { get; set; } = new();
+    public List<TileObject> Entropy { get; set; }
     public Vector2Int Coordinate { get; set; }
     public TileObject CurrentTile { get; set; }
     public bool IsStable { get; set; }
     public int EntropyStatus => Entropy.Count;
-    private void Start()
+    private void Awake()
     {
         Initialize();
-        foreach (var tile in Entropy)
-        {
-            Debug.Log(tile.Directions.ToStringCustom());
-        }
     }
     public void Initialize()
     {
+        Entropy = new();
         foreach (var tile in allTiles)
         {
             Entropy.Add(tile);
             TileObject rotate = new(tile);
             for (int i = 0; i < 3; i++)
             {
-                rotate.DirectionShift();
+                RotateTile(rotate);
                 Entropy.Add(new(rotate));
             }
         }
+    }
+    public void DebugStatus()
+    {
+        Debug.LogWarning($"Amount of entropy {EntropyStatus}");
+        Debug.LogWarning("Possibility Tiles: ");
+        foreach (var item in Entropy)
+        {
+            Debug.Log(item.Directions.ToStringCustom());
+        }
+    }
+    private void RotateTile(TileObject tile)
+    {
+        tile.DirectionShift();
+        transform.Rotate(0, 0, 90);
     }
 }
--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -1,7 +1,29 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
+    [SerializeField] private Cell prefab;
+
+    private readonly Dictionary<Vector2Int, Cell> tileAtPosition = new();
+    [field: SerializeField] public MazeGrid Grid { get; private set; }
+
+    private void Start()
+    {
+        Instantiate(prefab).DebugStatus();
+
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.black;
+        for (int x = 0; x < Grid.Length; x++)
+        {
+            for (int y = 0; y < Grid.Length; y++)
+            {
+                Vector3 pos = Grid.CoordinateToPosition(new Vector2Int(x, y));
+                Gizmos.DrawWireCube(pos, new Vector3(Grid.Size.x, Grid.Size.y, 0));
+            }
+        }
+    }
 }
