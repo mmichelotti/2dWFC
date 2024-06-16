b00000d20447782144c69e1e170bdd56d2038429
mmichelotti <m.michelotti@hotmail.it>
2024-06-16 10:23:48 +0200

    add entropy debugger

--- Assets/Scripts/Cell/CellDebugger.cs
+++ Assets/Scripts/Cell/CellDebugger.cs
@@ -6,4 +6,6 @@ public class CellDebugger : MonoBehaviour, IInitializable, IPositionable<Vector2
     [SerializeField] private TMP_Text tmpText;
     public Vector2Int Coordinate { get; set; }
     public void Init() => tmpText.text = Coordinate.ToString();
+
+    public void Set(string text) => tmpText.text = text;
 }
--- Assets/Scripts/Cell/CellManager.cs
+++ Assets/Scripts/Cell/CellManager.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using static UnityEditor.PlayerSettings;
 
 [RequireComponent(typeof(Grid))]
 public class CellManager : Manager
@@ -10,7 +11,12 @@ public class CellManager : Manager
     [SerializeField, ConditionalHide("debugCell",true)] private CellDebugger debuggerCell;
     [SerializeField] private Directions startingPoint;
     private Grid grid;
+
+    //to fix this double dictionary
     public Dictionary<Vector2Int, Cell> cellAtPosition { get; private set; } = new();
+    public Dictionary<Vector2Int, CellDebugger> debuggerAtPosition { get; private set; } = new();
+
+
     private CellNeighborhood neighborhood;
 
     private void Start()
@@ -19,39 +25,55 @@ public class CellManager : Manager
         InitializeCells();
         ResetCells();
     }
+    private void UpdateText()
+    {
+        foreach (var (pos, cell) in debuggerAtPosition) cell.Set(cellAtPosition[pos].State.Density.ToString());
+        
+    }
     public void SetCell(Vector2Int pos)
     {
         Cell currentCell = cellAtPosition[pos];
-        DirectionsRequired neighbourRequires = neighborhood.GetDirectionsRequired(pos);
-        Directions outOfBounds = grid.Boundaries(pos);
-
-        currentCell.DirectionsRequired = neighbourRequires.Exclude(outOfBounds);
+        currentCell.DirectionsRequired = GetRequiredDirections(pos);
         currentCell.UpdateState();
         currentCell.CollapseState();
         currentCell.Debug();
         neighborhood.UpdateState(pos);
 
-        Vector2Int nextPos = neighborhood.LowestEntropy;
+        UpdateText();
 
 
         //automated version
         /*
+         * Vector2Int nextPos = neighborhood.LowestEntropy;
         if (cellAtPosition[nextPos].State.IsEntangled) return;
         SetCell(nextPos);
         */
-        
-    }
 
+    }
+    private DirectionsRequired GetRequiredDirections(Vector2Int pos)
+    {
+        DirectionsRequired neighbourRequires = neighborhood.GetDirectionsRequired(pos);
+        Directions outOfBounds = grid.Boundaries(pos);
+        return neighbourRequires.Exclude(outOfBounds);
+    }
     public void RemoveCell(Vector2Int pos)
     {
         Cell currentCell = cellAtPosition[pos];
         currentCell.ResetState();
+        currentCell.DirectionsRequired = GetRequiredDirections(pos);
+        currentCell.UpdateState();
         neighborhood.UpdateEntropy(pos);
+        UpdateText();
     }
     private void InitializeCell(Vector2Int pos, Transform parent)
     {
+        if (debugCell)
+        {
+            var debCell = debuggerCell.SpawnInGrid(grid, pos, parent);
+            debuggerAtPosition.Add(pos, debCell);
+        }
         var defCell = defaultCell.SpawnInGrid(grid, pos, parent);
-        if(debugCell) debuggerCell.SpawnInGrid(grid, pos, parent);
+
         cellAtPosition.Add(pos, defCell);
     }
     private void InitializeCells()
