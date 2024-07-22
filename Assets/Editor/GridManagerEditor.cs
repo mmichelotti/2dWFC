41fe4c9042b266ac856ccdf66b6b7dd9d5ecf4ab
Marco Michelotti <m.michelotti@hotmail.it>
2024-07-22 16:05:13 +0200

    minor

--- Assets/Editor/GridManagerEditor.cs
+++ Assets/Editor/GridManagerEditor.cs
@@ -6,15 +6,9 @@ public class GridManagerEditor : Editor
 {
     public override void OnInspectorGUI()
     {
-        // Draw the default inspector
         DrawDefaultInspector();
-
-        // Get the GridManager script instance
         CellGrid gridManager = (CellGrid)target;
-
-        // Add a space for better readability
         EditorGUILayout.Space();
-
 
         if (GUILayout.Button("AutoFill"))
         {
