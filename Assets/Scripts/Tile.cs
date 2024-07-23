93b23a9ecc5850bf5e362c481f024fb974d929a7
MarcyBigrock <m.michelotti@hotmail.it>
2024-07-23 16:15:12 +0200

    preparing to use list of connections

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -15,11 +15,14 @@ public struct Tile : IDirectionable, IFormattable, IProbable
     [field: SerializeField] public string Name { get; private set; }
     [field: SerializeField] public Sprite Sprite { get; private set; }
     [field: SerializeField, Range(0, 1)] public float Probability { get; set; }
-    [field: SerializeField] public Directions Directions { get; set; }
-    public Vector3 Rotation { get; set; }
     [field: SerializeField] public RotationOption PossibleRotations { get; set; }
+    [field: SerializeField] public Directions Directions { get; set; }
+    [field: SerializeField] public List<Directions> Connections { get; set; }
+    public Vector3 Rotation { get; set; }
 
-    public Tile(Tile tile) => (Name, Sprite, Directions, Rotation, PossibleRotations, Probability) = (tile.Name, tile.Sprite, tile.Directions, tile.Rotation, tile.PossibleRotations, tile.Probability);
+    public Tile(Tile tile) => 
+        (Name, Sprite, Directions, Rotation, PossibleRotations, Probability, Connections) = 
+        (tile.Name, tile.Sprite, tile.Directions, tile.Rotation, tile.PossibleRotations, tile.Probability, tile.Connections);
 
     private readonly static HashSet<Tile> calculatingConfigurations = new();
     public List<Tile> AllConfigurations
@@ -47,9 +50,14 @@ public struct Tile : IDirectionable, IFormattable, IProbable
     private void Rotate()
     {
         Directions = Directions.Bitshift(Shift.Right);
+
+        for (int i = 0; i < Connections.Count; i++)
+            Connections[i] = Connections[i].Bitshift(Shift.Right);
+
         Rotation += new Vector3(0, 0, 90);
     }
 
     public readonly bool HasDirection(Directions dir) => Directions.HasFlag(dir);
     public readonly string ToString(string format, IFormatProvider formatProvider) => $"{Name}, {Directions.ToStringCustom()} roads with {Rotation.z} degrees rotation.";
+
 }
