4f2d019f1575f3104571a19b41a18844b98ec35e
mmichelotti <m.michelotti@hotmail.it>
2024-08-01 19:19:26 +0200

    minor

--- Assets/CellBuilder/Scripts/Cell/CellGrid.cs
+++ Assets/CellBuilder/Scripts/Cell/CellGrid.cs
@@ -41,11 +41,7 @@ public class CellGrid : Manager
     private void Awake()
     {
         Grid = GetComponent<Grid>();
-        InitializeGrid();
-    }
 
-    private void InitializeGrid()
-    {
         GameObject group = new("Cells");
         group.transform.parent = transform;
 
@@ -58,8 +54,10 @@ public class CellGrid : Manager
             Cells.Add(pos, quantumCell);
         };
         initializeCell.MatrixLoop(Grid.Size);
+
         quantumGrid = new QuantumGrid(Cells);
     }
+
     public void SpawnCell(Vector2Int pos, int? index = null)
     {
         Cells[pos].UpdateState();
@@ -106,7 +104,6 @@ public class CellGrid : Manager
         HashSet<Vector2Int> processedCells = new();
         Vector2Int currentPos = Grid.GetCoordinatesAt(Directions2D.All);
 
-        // Process the initial position
         SpawnCell(currentPos);
         processedCells.Add(currentPos);
         while (true)
@@ -161,64 +158,3 @@ public class CellGrid : Manager
         return cellPrefab;
     }
 }
-
-/*
-[BurstCompile]
-public struct SpawnCellJob : IJobParallelFor
-{
-    [ReadOnly] public NativeArray<Vector2Int> Positions;
-
-    public SpawnCellJob(NativeArray<Vector2Int> positions) => (Positions) = (positions);
-
-    public void Execute(int index)
-    {
-        // The job does not directly interact with QuantumCell
-        // Instead, it just processes the positions
-        Vector2Int pos = Positions[index];
-        // Add any position-based calculations or operations here
-    }
-}
-
- //to put within CelLGrid
-private IEnumerator FillGridJobs()
-{
-    Stopwatch stopwatch = new();
-
-    // To handle re-entry, stop any running instances
-    StopCoroutine(nameof(FillGridJobs));
-
-    HashSet<Vector2Int> processedCells = new();
-    Vector2Int currentPos = Grid.GetCoordinatesAt(Directions2D.All);
-
-    while (processedCells.Count < Grid.Count)
-    {
-        NativeList<Vector2Int> positions = new(Allocator.Persistent);
-
-        // Collect positions to process
-        for (int i = 0; i < 64; i++)
-        {
-            if (processedCells.Contains(currentPos))
-            {
-                currentPos = quantumGrid.LowestEntropy;
-                continue;
-            }
-
-            positions.Add(currentPos);
-            processedCells.Add(currentPos);
-            currentPos = quantumGrid.LowestEntropy;
-        }
-
-        SpawnCellJob spawnCellJob = new(positions);
-
-        JobHandle jobHandle = spawnCellJob.Schedule(positions.Length, 1);
-        yield return new WaitUntil(() => jobHandle.IsCompleted);
-
-        jobHandle.Complete();
-        foreach (var pos in positions) SpawnCell(pos);
-        positions.Dispose();
-
-        yield return new WaitForEndOfFrame();  // Ensure coroutine continues in the next frame
-    }
-    areJobsCompleted = true;
-}
-*/
